83258de6fbb8a77a5e794453f107bd59857458d9
ae-dschorsaanjo <grzzltn@gmail.com>
2017-01-27 17:47:40 +0100

    Slightly modified and reordered code

--- htcc.php
+++ htcc.php
@@ -1,6 +1,88 @@
 <?php
+$_values = array(
+	array(1 => "non-existent", 2 => "nem létező", 3 => "00", 4 => "00 - non-existent", 5 => "00 - nem létező"),
+	array(1 => "disastrous", 2 => "katasztrofális", 3 => "01", 4 => "01 - disastrous", 5 => "01 - katasztrofális"),
+	array(1 => "wretched", 2 => "pocsék", 3 => "02", 4 => "02 - wretched", 5 => "02 - pocsék"),
+	array(1 => "poor", 2 => "csapnivaló", 3 => "03", 4 => "03 - poor", 5 => "03 - csapnivaló"),
+	array(1 => "weak", 2 => "gyenge", 3 => "04", 4 => "04 - weak", 5 => "04 - gyenge"),
+	array(1 => "inadequate", 2 => "középszerű", 3 => "05", 4 => "05 - inadequate", 5 => "05 - középszerű"),
+	array(1 => "passable", 2 => "megfelelő", 3 => "06", 4 => "06 - passable", 5 => "06 - megfelelő"),
+	array(1 => "solid", 2 => "jó", 3 => "07", 4 => "07 - solid", 5 => "07 - jó"),
+	array(1 => "excellent", 2 => "remek", 3 => "08", 4 => "08 - excellent", 5 => "08 - remek"),
+	array(1 => "formidable", 2 => "nagyszerű", 3 => "09", 4 => "09 - formidable", 5 => "09 - nagyszerű"),
+	array(1 => "outstanding", 2 => "kiemelkedő", 3 => "10", 4 => "10 - outstanding", 5 => "10 - kiemelkedő"),
+	array(1 => "brilliant", 2 => "ragyogó", 3 => "11", 4 => "11 - brilliant", 5 => "11 - ragyogó"),
+	array(1 => "magnificent", 2 => "lenyűgöző", 3 => "12", 4 => "12 - magnificent", 5 => "12 - lenyűgöző"),
+	array(1 => "world class", 2 => "világklasszis", 3 => "13", 4 => "13 - world class", 5 => "13 - világklasszis"),
+	array(1 => "supernatural", 2 => "természetfeletti", 3 => "14", 4 => "14 - supernatural", 5 => "14 - természetfeletti"),
+	array(1 => "titanic", 2 => "titáni", 3 => "15", 4 => "15 - titanic", 5 => "15 - titáni"),
+	array(1 => "extra-terrestrial", 2 => "földöntúli", 3 => "16", 4 => "16 - extra-terrestrial", 5 => "16 - földöntúli"),
+	array(1 => "mythical", 2 => "legendás", 3 => "17", 4 => "17 - mythical", 5 => "17 - legendás"),
+	array(1 => "magical", 2 => "varázslatos", 3 => "18", 4 => "18 - magical", 5 => "18 - varázslatos"),
+	array(1 => "utopian", 2 => "csodás", 3 => "19", 4 => "19 - utopian", 5 => "19 - csodás"),
+	array(1 => "divine", 2 => "isteni", 3 => "20", 4 => "20 - divine", 5 => "20 - isteni"),
+);	
 
 define("newCell", "</td><td>", false);
+
+define("maxExp", 20, true);
+define("maxLead", 8, true);
+define("minIndex", 0, true);
+define("minSize", 1, true);		
+define("maxSize", 50, true);
+define("minJersey", 1, true);
+define("maxJersey", 99, true);
+define("defaultSize", 16, true);
+
+define("ENG", 1, true);
+define("HUN", 2, true);
+define("NUM", 3, true);
+define("NUM_ENG", 4, true);
+define("NUM_HUN", 5, true);
+
+const modes = array(
+	array(ENG, 'English'),
+	array(HUN, 'Magyar'),
+	array(NUM, '#'),
+	array(NUM_ENG, '# - English'),
+	array(NUM_HUN, '# - Magyar')
+);
+
+/**
+ * Returns the mode options.
+ */
+function getModes($selected) {
+	$out = '';
+	echo $selected;
+	for ($i = 0, $max = count(modes); $i < $max; $i++)
+		$out .= '<option value="' . modes[$i][0] . '"' . ($selected == modes[$i][0] ? " selected" : '') . '>' . modes[$i][1] . '</option>';
+	return $out;
+}
+
+/**
+ * Returns a long-long string with options (HTML <option> tags).
+ *
+ * @param $min Minimum index
+ * @param $max Maximum index
+ * @param $selected The selected index by default
+ * @param $needvalues If it is not false, it'll write the needed type of value
+ * @return The needed options as string.
+ */
+function getOptions($min, $max, $selected = 0, $needvalues = 1) {
+	
+	if ($needvalues)
+		global $_values;
+	
+	$out = '';
+	for ($i = $min; $i <= $max; $i++)
+		$out .= "<option value='" . $i . ($i == $selected ? "' selected " : "'") . ">" . ($needvalues ? $_values[$i][$needvalues] : $i) . "</option>\n";
+	return $out;
+}
+
+function check($s) {
+	
+	return htmlspecialchars(stripslashes(trim($s)));
+}
 
 class Player {
 	private $id;
@@ -28,31 +110,110 @@ class Player {
 	}
 }
 
-function check($s) {
-	
-	return htmlspecialchars(stripslashes(trim($s)));
+function writeTable() {
+	echo "<script src='sorttable.js'></script>\n" .
+		"<table class='sortable'>" .
+		"<caption>" . check($_POST['teamname']) . "</caption>" .
+		"<tr>" .
+		"<th>#</th>" .
+		"<th>Name</th>" .
+		"<th>Experience</th>" .
+		"<th>Leadership</th>" .
+		"<th>TCV</th>" .
+		"</tr>";
+
+	for ($i = minSize; $i <= check($_POST['teamsize']); $i++) {
+		$team[$i] = new Player($i,
+		                       check($_POST['pname' . $i]),
+							   check($_POST['pnum' . $i]),
+							   check($_POST['pexp' . $i]),
+							   check($_POST['plead' . $i]));
+		$team[$i]->outF();
+	}
+
+	echo "</table>";
 }
 
 // ---------- main ---------- //
 
-echo "<script src='sorttable.js'></script>\n" .
-	"<table class='sortable'>" .
-	"<caption>" . check($_POST['teamname']) . "</caption>" .
-	"<tr>" .
-	"<th>#</th>" .
-	"<th>Name</th>" .
-	"<th>Experience</th>" .
-	"<th>Leadership</th>" .
-	"<th>TCV</th>" .
-	"</tr>";
+?>
 
-for ($i = minSize; $i <= check($_POST['teamsize']); $i++) {
-	$team[$i] = new Player($i, check($_POST['pname' . $i]), check($_POST['pnum' . $i]), check($_POST['pexp' . $i]), check($_POST['plead' . $i]));
-	$team[$i]->outF();
-}
+<script>
+	const maxExp = <?php echo maxExp; ?>;
+	const maxLead = <?php echo maxLead; ?>;
+	const minIndex = <?php echo minIndex; ?>;
+	const minSize = <?php echo minSize; ?>;
+	const maxSize = <?php echo maxSize; ?>;
+	const minJersey = <?php echo minJersey; ?>;
+	const maxJersey = <?php echo maxJersey; ?>;
+	const defaultSize = <?php echo (isset($_POST['teamsize']) ? $_POST['teamsize'] : defaultSize); ?>;
+	const isPosted = <?php echo (int)($_SERVER['REQUEST_METHOD'] == 'POST') ?>;
+</script>
+<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+<script src="htcc.js"></script>
 
-echo "</table>";
+<div class='body'>
 
-/*
-for($i = 0; $i < count($_values[0]); $i++) 
-    echo $i." ".$_values[1][$i]."<br>";*/
+<h1>Hattrick (Team) Captain Chooser</h1>
+
+<form method="post">
+	Team's name: <input type='text' name='teamname' value='<?php echo isset($_POST['teamname']) ? $_POST['teamname'] : ''; ?>' required />&nbsp;&nbsp;&nbsp;&nbsp;
+	Team's size: <input type='number' name='teamsize' min='0' max='99' value='<?php echo isset($_POST['teamsize']) ? $_POST['teamsize'] : 16; ?>' />
+	Values: <select name='valuesmode'><?php
+		echo getModes($_POST['valuesmode']);
+	?></select>
+	<!-- <input type='checkbox' name='teamfilecheck' />
+	Team's file: <input type='file' name='teamfile' disabled /><input type='button' value='Read' name='teamfileread' disabled /> -->
+	<input type='submit' name='generate' value='Generate' />
+</form>
+
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if (!isset($_POST['data'])) {
+?>
+
+<form method='post'>
+	<input name='teamname' value='<?php echo $_POST['teamname']; ?>' hidden />
+	<input name='teamsize' value='<?php echo $_POST['teamsize']; ?>' hidden />
+	<table class='players'>
+		<tr class='head'>
+			<th>id</th>
+			<th>Name</th>
+			<th>Experience</th>
+			<th>Leadership</th>
+			<th>Num</th>
+		</tr>
+		<!-- <tr><td class="refresh" colspan="5">Refreshing is quiet slow, please wait a bit.</td></tr> -->
+		<tr><td class='small'>&nbsp;</td></tr>
+
+		<?php
+			for ($i = 1; $i <= 50; $i++) {
+				echo "<tr name='tr" . $i . "'><td class='right'>" .
+					$i . 
+					"</td><td><input type='text' name='pname$i' />\n</td><td><select class='gen' name='pexp$i'>\n" . 
+					getOptions(minIndex, maxExp, 0, $_POST['valuesmode']) .
+					"</select>" .
+					"</td><td><select class='gen' name='plead$i'>\n" . 
+					getOptions(minIndex, maxLead, 0, $_POST['valuesmode']) .
+					"</select>\n" .
+					"</td><td><input type='number' name='pnum$i' min='" . minJersey . "' max='" . maxJersey . "' value='$i'</td></tr>\n\n";
+			}
+		?>
+		<tr><td colspan='5'>
+		<div class='submit'><input type='submit' name='data' value='Send'/></div>
+		</td></tr>
+	</table>
+</form>
+
+<?php
+		}
+		else {
+			writeTable();
+		}
+	}
+	else {
+		//echo "There's nothing to show.";
+	};
+?>
+
+</div> <!-- end of div.body -->
--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <!DOCTYPE html>
-<?php
-	$_values = array( 
-		array( "nem létező", "katasztrofális", "pocsék", "csapnivaló", "gyenge", "középszerű", "megfelelő", "jó", "remek", "nagyszerű", "kiemelkedő", "ragyogó", "lenyűgöző", "világklasszis", "természetfeletti", "titáni", "földöntúli", "legendás", "varázslatos", "csodás", "isteni" ), 
-		array( "non-existent", "disastrous", "wretched", "poor", "weak", "inadequate", "passable", "solid", "excellent", "formidable", "outstanding", "brilliant", "magnificent", "world class", "supernatural", "titanic", "extra-terrestrial", "mythical", "magical", "utopian", "divine" ), 
-		array( "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" )
-	);
-	
-	define("maxExp", 20, true);
-	define("maxLead", 8, true);
-	define("minIndex", 0, true);
-	define("minSize", 1, true);		
-	define("maxSize", 50, true);
-	define("minJersey", 1, true);
-	define("maxJersey", 99, true);
-	define("defaultSize", 16, true);
-	
-	function getOptions($min, $max, $selected = 0, $needvalues = true) {
-		
-		if ($needvalues)
-			global $_values;
-		$out = '';
-		for ($i = $min; $i <= $max; $i++)
-			$out .= "<option value='" . $i . ($i == $selected ? "' selected " : "'") . ">" . ($needvalues ? $_values[1][$i] : $i) . "</option>\n";
-		return $out;
-	}
-?>
 <html>
 	<head>
-		<title>Hattrick Captain Chooser</title>
+		<title>Hattrick Captain Chooser DEMO</title>
 		<meta name="generator" content="Bluefish 2.2.6" >
 		<meta name="author" content="root" >
 		<meta name="copyright" content="">
@@ -35,73 +9,13 @@
 		<meta name="description" content="Hattrick (Team) Captain Chooser, because we are listening to every single details!">
 		<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
 		<link rel="stylesheet" type="text/css" href="htcc.css" />
-		<script>
-			const maxExp = <?php echo maxExp; ?>;
-			const maxLead = <?php echo maxLead; ?>;
-			const minIndex = <?php echo minIndex; ?>;
-			const minSize = <?php echo minSize; ?>;
-			const maxSize = <?php echo maxSize; ?>;
-			const minJersey = <?php echo minJersey; ?>;
-			const maxJersey = <?php echo maxJersey; ?>;
-			const defaultSize = <?php echo defaultSize; ?>;
-			const isPosted = <?php echo (int)($_SERVER['REQUEST_METHOD'] == 'POST') ?>;
-		</script>
-		<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
-		<script src="htcc.js"></script>
 	</head>
 	<body>
-	<table class='body'><tr><td>
+	<?php
+	require_once "htcc.php";
+	?>
 	
-		<h1>Hattrick (Team) Captain Chooser</h1>
-		<form  method="post"><!-- action="index.php" -->
-			Team's name: <input type='text' name='teamname' />&nbsp;&nbsp;&nbsp;&nbsp;
-			Team's size: <select name='teamsize'><?php echo getOptions(minSize, maxSize, defaultSize, false); ?></select>
-			Values: <select name='valuesmode'>
-				<option value='1'>English</option>
-				<option value='0'>Hungarian</option>
-				<option value='2'>#</option>
-			</select>
-			<input type='button' id='help' value="?" />
-			<br>
-		    <!-- <input type='checkbox' name='teamfilecheck' />
-		    Team's file: <input type='file' name='teamfile' disabled /><input type='button' value='Read' name='teamfileread' disabled /> -->
-			<table class='players'>
-				<tr class='head'>
-					<th>id</th>
-					<th>Name</th>
-					<th>Experience</th>
-					<th>Leadership</th>
-					<th>Num</th>
-				</tr>
-				<!-- <tr><td class="refresh" colspan="5">Refreshing is quiet slow, please wait a bit.</td></tr> -->
-				<tr><td class='small'>&nbsp;</td></tr>
-				<?php
-					for ($i = 1; $i <= 50; $i++) {
-						echo "<tr name='tr" . $i . "'><td class='right'>" .
-							$i . 
-							"</td><td><input type='text' name='pname" . 
-							$i . 
-							"' />\n</td><td><select name='pexp" . $i . "'>\n" . 
-							getOptions(minIndex, maxExp) .
-							"</select>" .
-							"</td><td><select name='plead" . $i . "'>\n" . 
-							getOptions(minIndex, maxLead) .
-							"</select>\n" .
-							"</td><td><select name='pnum" . $i . "'>\n" .
-							getOptions(minJersey, maxJersey, $i, false) .
-							"</select></td></tr>\n\n";
-					}
-				?>
-				<tr><td colspan='5'><div class='submit'><input type='submit' /></div></td></tr>
-			</table>
-		</form>
-		<?php
-			if ($_SERVER['REQUEST_METHOD'] == 'POST')
-				require "htcc.php";
-			else
-				echo "There's nothing to show.";
-		?>
-	</td></tr></table>
 	</body>
 </html>
